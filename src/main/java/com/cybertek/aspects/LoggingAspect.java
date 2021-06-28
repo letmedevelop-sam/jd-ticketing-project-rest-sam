3a143e2f0b0104cdfd7ab4ef73825a5e2476aaa7
SamKar <letmedevelopsam@gmail.com>
2021-06-28 23:52:37 +0300

    jd ticketing project rest sam AOP LoggingAspect added

--- src/main/java/com/cybertek/aspects/LoggingAspect.java
+++ src/main/java/com/cybertek/aspects/LoggingAspect.java
@@ -1,17 +1,12 @@
 package com.cybertek.aspects;
 
-
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Aspect
 @Configuration
@@ -60,7 +55,6 @@ public class LoggingAspect {
     //initialize the logger
     Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-
     // @Pointcut sets : Where-when-what to implement in the code
     @Pointcut("execution(* com.cybertek.controller.ProjectController.*(..))|| execution(* com.cybertek.controller.TaskController.*(..))")   //each method - everywhere in the controller
     private void anyControllerOperation(){}
@@ -70,6 +64,17 @@ public class LoggingAspect {
         logger.info("Before(User: {} Method : {} - Parameters : {}", auth.getName(), joinPoint.getSignature().toShortString(), joinPoint.getArgs());
     }
 
+    @AfterReturning(pointcut = "anyControllerOperation()",returning = "results")
+    public void anyAfterReturningControllerOperationAdvice(JoinPoint joinPoint,Object results){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        logger.info("AfterReturning(User : {} Method : {} - Results : {}",auth.getName(),joinPoint.getSignature().toShortString(),results.toString());
+    }
+
+    @AfterThrowing(pointcut = "anyControllerOperation()",throwing = "exception")
+    public void anyAfterThrowingControllerOperationAdvice(JoinPoint joinPoint,RuntimeException exception){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        logger.info("After throwing(User : {} Method : {} - Exception : {}",auth.getName(),joinPoint.getSignature().toShortString(),exception.getLocalizedMessage());
+    }
 
 
 }
